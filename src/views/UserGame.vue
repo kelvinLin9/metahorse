25c54b5e575b9ecf55376c618169f1fc8c76d626
kelvinLin9 <kelvin80121@hotmail.com>
2022-08-14 11:14:45 +0800

    update
    遊戲更新

--- src/views/UserGame.vue
+++ src/views/UserGame.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="container-fluid"
-        v-for="item in horses" :key="item.color">
-    <div :class="[item.color, item.speed]">
+  <div class="container-fluid">
+    <div 
+    v-for="item in horses" :key="item.id"
+    @click.prevent="selectedHorse(item)"
+    :class="[item.color, item.speed, { 'paused': isPlay }]">
+      {{ item.id }}
       <font-awesome-icon icon="fa-solid fa-horse" />
+      <span class="fs-5">
+        {{ item.speed }}
+      </span>
     </div>
 
     <!-- <div class="Q1">cubic-bezier</div> -->
     <!-- <div class="Q2">cubic-bezier</div> -->
     <span class="line"></span>
-    <button type="button"
+    <bottom type="button"
             class="btn btn-primary btn-hover rounded-0 fw-bold btn-lg fs-3"
             @click.prevent="play">
+
+    <span>
       開始
-    </button>
+    </span> 
+    <span>
+      你選擇了 {{ horse.id }} 號
+    </span>
+    </bottom>
+
   </div>
 </template>
 
@@ -28,21 +41,27 @@ export default {
         },
         {
           color: 'text-secondary',
-          speed: 'ease'
+          speed: 'ease',
+          id: 2
         },
         {
           color: 'text-success',
-          speed: 'ease-in'
+          speed: 'ease-in',
+          id: 3
         },
         {
           color: 'text-danger',
-          speed: 'ease-out'
+          speed: 'ease-out',
+          id: 4
         },
         {
           color: '',
-          speed: 'ease-in-out'
+          speed: 'ease-in-out',
+          id: 5
         }
       ],
+      horse: [],
+      horsesX: {},
       isPlay: false
     }
   },
@@ -50,56 +69,74 @@ export default {
     shuffleArray (inputArray) {
       inputArray.sort(() => Math.random() - 0.5)
     },
+    selectedHorse(item) {
+      this.horse = { ...item}
+    },
     play () {
-      const array = ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out']
-      this.shuffleArray(array)
-      console.log(array)
-      this.horses.forEach((item, index) => {
-        item.speed = array[index]
-      })
-      console.log(this.horses)
+      // const array = ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out']
+      // // let array = [1, 2, 3, 4, 5]
+      // this.shuffleArray(array)
+      // console.log(array)
+      // this.horses.forEach((item, index) => {
+      //   item.speed = array[index]
+      // })
+      // console.log(this.horses)
       this.isPlay = true
     }
   },
-  // mounted () {
-
-  // }
+  created () {
+    let array = ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out']
+    this.shuffleArray(array)
+    console.log(array)
+    this.horses.forEach((item, index) => {
+      item.speed = array[index]
+    })
+    console.log(this.horses)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 div{
   display:flex;
-  justify-content:center;
+  // justify-content:center;
   align-items:center;
   position:absolute;
   font-size:50px;
   text-align:center;
 }
+.paused{
+  animation:move 10s ;
+}
 .linear{
   top:30px;
-  animation:move 10s linear ;
-  animation-fill-mode : forwards;
+  animation-timing-function: linear;
+  animation-fill-mode : forwards;  
 }
 .ease{
   top:100px;
-  animation:move 10s ease ;
+  // animation:move 10s ease ;
+  animation-timing-function: ease;
   animation-fill-mode : forwards;
 }
 .ease-in{
   top:170px;
-  animation:move 10s ease-in ;
+  // animation:move 10s ease-in ;
+  animation-timing-function: ease-in;
   animation-fill-mode : forwards;
 }
 .ease-out{
   top:240px;
-  animation:move 10s ease-out ;
+  // animation:move 10s ease-out ;
+  animation-timing-function: ease-out;
   animation-fill-mode : forwards;
 }
 .ease-in-out{
   top:310px;
-  animation:move 10s ease-in-out ;
+  // animation:move 10s ease-in-out ;
+  animation-timing-function: ease-in-out;
   animation-fill-mode : forwards;
+  
 }
 @keyframes move{
     0%{
@@ -120,9 +157,13 @@ div{
   border-right:2px solid red;
 }
 .btn{
-  display:block;
-  // z-index:10;
+  // display:block;
+  z-index:10;
   position:absolute;
   top: 450px;
+    &:hover {
+      color: #333;
+      animation-play-state: running;
+    }
 }
 </style>
