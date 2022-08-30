b6ce38df079be1638e3c5dee2c4343528237c06c
kelvinLin9 <kelvin80121@hotmail.com>
2022-08-30 14:51:27 +0800

    後端面板更新

--- src/stores/chartStore.js
+++ src/stores/chartStore.js
@@ -9,23 +9,58 @@ const status = statusStore()
 export default defineStore('productStore', {
   state: () => ({
     orders: {},
+    allOrders: [],
+    ordersNum: 0,
     isNew: false,
     pagination: {},
     isLoading: false,
     tempOrder: {},
-    currentPage: 1
+    currentPage: 1,
+    revenue: 0
   }),
   actions: {
+    getAllOrders () {
+      this.allOrders = []
+      this.revenue = 0
+      this.ordersNum = 0
+      for (let i = 1; i <= this.pagination.total_pages; i++){
+        const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders?page=${i}`
+        this.isLoading = true
+        axios.get(url).then((response) => {
+          this.allOrders[i-1] = response.data.orders
+          response.data.orders.forEach((item) => {
+            this.revenue += item.total
+            this.ordersNum += 1
+            console.log(this.revenue)
+          })
+          this.isLoading = false   
+          console.log(this.allOrders)       
+        })
+      }
+      
+
+
+    },
     getOrders (currentPage = 1) {
       this.currentPage = currentPage
       const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders?page=${currentPage}`
       this.isLoading = true
-      this.$http.get(url, this.tempProduct).then((response) => {
+      axios.get(url, this.tempProduct).then((response) => {
         this.orders = response.data.orders
         this.pagination = response.data.pagination
         this.isLoading = false
         console.log(response)
+        this.getAllOrders ()
       })
+    },
+    calRevenue () {
+      this.revenue = 0
+      console.log(this.pagination.total_pages)
+      this.orders.forEach((item) => {
+        this.revenue += item.total
+        console.log(this.revenue)
+      })
+    
     }
   }
 })
--- src/views/admin/AdminHome.vue
+++ src/views/admin/AdminHome.vue
@@ -9,19 +9,19 @@
   <div class="p-4">
     <!-- 統計資料 -->
     <div class="row gy-3 mb-4">
-      <div class="col">
+      <!-- <div class="col">
         <div class="card shadow-sm">
           <div class="card-body text-end text-nowrap">
             <h2 class="display-6">在線人數</h2>
             <p class="display-4">3,456</p>
           </div>
         </div>
-      </div>
+      </div> -->
       <div class="col">
         <div class="card shadow-sm">
           <div class="card-body text-end text-nowrap">
             <h2 class="display-6">訂單數</h2>
-            <p class="display-4">1,092</p>
+            <p class="display-4">{{ ordersNum }}</p>
           </div>
         </div>
       </div>
@@ -29,7 +29,7 @@
         <div class="card shadow-sm">
           <div class="card-body text-end text-nowrap">
             <h2 class="display-6">營業額</h2>
-            <p class="display-4"><span class="fs-4">NT$</span>142,456</p>
+            <p class="display-4"><span class="fs-4">NT$</span>{{ $filters.currency(revenue) }}</p>
           </div>
         </div>
       </div>
@@ -68,55 +68,55 @@
           <table class="table table-hover mb-0 text-nowrap">
             <thead class="bg-light border-bottom-3 fw-bold">
               <tr class="align-middle">
-                <th scope="col" class="ps-3 py-3">訂單編號</th>
+                <th scope="col" class="ps-3 py-3">購買時間</th>
+                <th scope="col">訂單編號</th>
                 <th scope="col">Email</th>
                 <th scope="col">用戶姓名</th>
                 <th scope="col">購買品項</th>
-                <th scope="col">付款狀態</th>
                 <th scope="col">購買金額</th>
-                <th scope="col" class="pe-3">編輯</th>
+                <th scope="col">付款狀態</th>
+                <th scope="col" class="pe-3">編輯 / 刪除</th>
               </tr>
             </thead>
             <tbody>
-              <tr class="align-middle">
-                <td scope="row">TX123456788</td>
-                <td>ab1234@email.com</td>
-                <td>卡小伯</td>
-                <td>六角西餐：大麥克全家餐</td>
-                <td class="text-danger">尚未付款</td>
-                <td class="text-end pe-3">900</td>
-                <td>
-                  <a href="#" class="btn btn-outline-dark">
-                    編輯<i class="bi bi-pencil-square"></i>
-                  </a>
-                </td>
-              </tr>
-              <tr class="align-middle">
-                <td scope="row">TX123456788</td>
-                <td>ab1234@email.com</td>
-                <td>卡小伯</td>
-                <td>六角西餐：大麥克全家餐</td>
-                <td class="text-danger">尚未付款</td>
-                <td class="text-end pe-3">900</td>
-                <td>
-                  <a href="#" class="btn btn-outline-dark">
-                    編輯<i class="bi bi-pencil-square"></i>
-                  </a>
-                </td>
-              </tr>
-              <tr class="align-middle">
-                <td scope="row">TX123456788</td>
-                <td>ab1234@email.com</td>
-                <td>卡小伯</td>
-                <td>六角西餐：大麥克全家餐</td>
-                <td class="text-danger">尚未付款</td>
-                <td class="text-end pe-3">900</td>
-                <td>
-                  <a href="#" class="btn btn-outline-dark">
-                    編輯<i class="bi bi-pencil-square"></i>
-                  </a>
-                </td>
-              </tr>
+              <template v-for="(item, key) in orders" :key="key">
+                <tr v-if="orders.length"
+                    :class="{'text-danger': !item.is_paid}">
+                  <td>{{ $filters.date(item.create_at) }}</td>
+                  <td>{{ item.id }}</td>
+                  <td><span v-text="item.user.email" v-if="item.user"></span></td>
+                  <td><span v-text="item.user.name" v-if="item.user"></span></td>
+                  <td>
+                    <ul class="list-unstyled">
+                      <li v-for="(product, i) in item.products" :key="i">
+                        {{ product.product.title }} 數量：{{ product.qty }} / 
+                        {{ product.product.unit }}
+                      </li>
+                    </ul>
+                  </td>
+                  <td class="text-right">{{ $filters.currency(item.total) }}</td>
+                  <td>
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" :id="`paidSwitch${item.id}`"
+                             v-model="item.is_paid"
+                             @change="updatePaid(item)">
+                      <label class="form-check-label" :for="`paidSwitch${item.id}`">
+                        <span v-if="item.is_paid">已付款</span>
+                        <span v-else>未付款</span>
+                      </label>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="btn-group">
+                      <button class="btn btn-outline-primary btn-sm"
+                              @click="openModal(false, item)">檢視</button>
+                      <button class="btn btn-outline-danger btn-sm"
+                              @click="openDelOrderModal(item)"
+                      >刪除</button>
+                    </div>
+                  </td>
+                </tr>
+              </template>
             </tbody>
           </table>
         </div>
@@ -141,104 +141,100 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'pinia'
+import chartStore from '@/stores/chartStore'
 import ProductModal from '@/components/admin/ProductModal.vue'
 import Pagination from '@/components/admin/Pagination.vue'
 import DelModal from '@/components/admin/DelModal.vue'
 
 export default {
-  data () {
-    return {
-      products: [], // 所有產品資訊
-      pagination: {}, // 分頁資訊
-      // 先傳給ProductModal，ProductModal接收後儲存起來
-      tempProduct: {}, // 主要為了編輯
-      isNew: false, // 是否新增
-      isLoading: false
-    }
-  },
-  // 區域註冊
   components: {
     ProductModal,
     DelModal,
     Pagination
   },
-  inject: ['emitter'],
+  computed: {
+    ...mapState(chartStore, ['orders', 'revenue', 'allOrders', 'ordersNum'])
+  },
   methods: {
+    ...mapActions(chartStore, ['getOrders'])
     // 在生命週期觸發
-    getProducts (page = 1) {
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products/?page=${page}`
-      this.isLoading = true
-      // 不需要加入資料 get(路徑)就可以了
-      this.$http.get(api).then((res) => {
-        this.isLoading = false
-        if (res.data.success) {
-          console.log(1, res.data)
-          this.products = res.data.products
-          this.pagination = res.data.pagination
-        }
-      })
-    },
+    // getProducts (page = 1) {
+    //   const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products/?page=${page}`
+    //   this.isLoading = true
+    //   // 不需要加入資料 get(路徑)就可以了
+    //   this.$http.get(api).then((res) => {
+    //     this.isLoading = false
+    //     if (res.data.success) {
+    //       console.log(1, res.data)
+    //       this.products = res.data.products
+    //       this.pagination = res.data.pagination
+    //     }
+    //   })
+    // },
     // 新增(isNew)，tempProduct清空
     // 編輯(!isNew)，tempProduct = item
-    openModal (isNew, item) {
-      if (isNew) {
-        this.tempProduct = {}
-      } else {
-        this.tempProduct = { ...item }
-      }
-      this.isNew = isNew
-      const productComponent = this.$refs.productModal
-      productComponent.showModal()
-    },
+    // openModal (isNew, item) {
+    //   if (isNew) {
+    //     this.tempProduct = {}
+    //   } else {
+    //     this.tempProduct = { ...item }
+    //   }
+    //   this.isNew = isNew
+    //   const productComponent = this.$refs.productModal
+    //   productComponent.showModal()
+    // },
     // 把外層資料儲存下來，再傳送到遠端
-    updateProduct (item) {
-      // console.log(item)
-      this.tempProduct = item
-      // 新增
-      let api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
-      let httpMethod = 'post'
-      // 編輯
-      if (!this.isNew) {
-        api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`
-        httpMethod = 'put'
-      }
-      const productComponent = this.$refs.productModal
-      this.$http[httpMethod](api, { data: this.tempProduct }).then((response) => {
-        console.log(response)
-        productComponent.hideModal()
-        if (response.data.success) {
-          this.getProducts()
-          this.emitter.emit('push-message', {
-            style: 'success',
-            title: '更新成功'
-          })
-        } else {
-          this.emitter.emit('push-message', {
-            style: 'danger',
-            title: '更新失敗',
-            content: response.data.message.join('、')
-          })
-        }
-      })
-    },
+    // updateProduct (item) {
+    //   // console.log(item)
+    //   this.tempProduct = item
+    //   // 新增
+    //   let api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+    //   let httpMethod = 'post'
+    //   // 編輯
+    //   if (!this.isNew) {
+    //     api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`
+    //     httpMethod = 'put'
+    //   }
+    //   const productComponent = this.$refs.productModal
+    //   this.$http[httpMethod](api, { data: this.tempProduct }).then((response) => {
+    //     console.log(response)
+    //     productComponent.hideModal()
+    //     if (response.data.success) {
+    //       this.getProducts()
+    //       this.emitter.emit('push-message', {
+    //         style: 'success',
+    //         title: '更新成功'
+    //       })
+    //     } else {
+    //       this.emitter.emit('push-message', {
+    //         style: 'danger',
+    //         title: '更新失敗',
+    //         content: response.data.message.join('、')
+    //       })
+    //     }
+    //   })
+    // },
     // 開啟刪除 Modal
-    openDelProductModal (item) {
-      this.tempProduct = { ...item }
-      const delComponent = this.$refs.delModal
-      delComponent.showModal()
-    },
-    delProduct () {
-      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${this.tempProduct.id}`
-      this.$http.delete(url).then((response) => {
-        console.log(response.data)
-        const delComponent = this.$refs.delModal
-        delComponent.hideModal()
-        this.getProducts()
-      })
-    }
+    // openDelProductModal (item) {
+    //   this.tempProduct = { ...item }
+    //   const delComponent = this.$refs.delModal
+    //   delComponent.showModal()
+    // },
+  //   delProduct () {
+  //     const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${this.tempProduct.id}`
+  //     this.$http.delete(url).then((response) => {
+  //       console.log(response.data)
+  //       const delComponent = this.$refs.delModal
+  //       delComponent.hideModal()
+  //       this.getProducts()
+  //     })
+  //   }
   },
+
   created () {
-    this.getProducts()
+    this.getOrders()
+    // this.getProducts()
   }
 }
 </script>
