d474f51abfc8f7c20f4864857495f69f426d7c8f
kelvin <kelvin80121@hotmail.com>
2022-09-12 18:56:15 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,17 +4,6 @@ module.exports = defineConfig({
 
   // 修正檔案命名問題
   lintOnSave: false,
-
-  publicPath: '/metahorse/dist'
+  publicPath: process.env.NODE_ENV === 'production' ? '/metahorse/' : '/'
 })
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' ? '/metahorse/' : '/'
-// }
-
-// place it with same level of package.json
-// vue.config.js 設定指南 : https://cli.vuejs.org/zh/config/#%E5%85%A8%E5%B1%80-cli-%E9%85%8D%E7%BD%AE
-// module.exports = {
-//   // publicPath 為 project-name，根目錄地址為上傳的網域
-//   publicPath: '/metahorse/'
-// }
