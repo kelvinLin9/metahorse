10271fa1921bca039ccb18bb6f64f48ed509a633
kelvinLin9 <kelvin80121@hotmail.com>
2022-10-03 13:46:18 +0800

    刪除多於註解 console

--- src/stores/statusStore.js
+++ src/stores/statusStore.js
@@ -10,7 +10,6 @@ export default defineStore('statusStore', {
     // 傳到ToastMessages.vue
     // 抽獎頁面的提示合併過來一起用
     PushManager (res = true, title = '更新', content) {
-      console.log(res, title, content)
       if (res) {
         const obj = {
           style: 'success',
@@ -19,17 +18,12 @@ export default defineStore('statusStore', {
         }
         this.messages.push(obj)
       } else {
-        // 有些訊息是字串，有些則是陣列，統一格式
-        // const message = typeof res.data.message === 'string'
-        //   ? [res.data.message]
-        //   : res.data.message
         const obj = {
           style: 'danger',
           title: `${title}失敗`,
           content: `${content}`
         }
         this.messages.push(obj)
-        console.log(this.messages)
       }
     }
   }
